1a06115b865008e159d074dc1cfa16863d0b3567
Rohit-231 <pal8035@gmail.com>
2023-02-28 22:11:27 +0530

    updating routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const router=express.Router()
 
 
 router.post('/signup',authController.signup)
-router.post('/login',authController.login)
+router.get('/login',authController.login)
 
 router.post('/forgotPassword',authController.forgotPassword)
 router.patch('/resetPassword/:token',authController.resetPassword)
